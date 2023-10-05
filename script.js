3694139daaa1e613d8de9d2dfb9cb709458ad509
Ritanjit Das <127327985+Ritanjit@users.noreply.github.com>
2023-10-05 18:01:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let string ="";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == '←'){
+            string = slice(0,-1);
+            document.querySelector('input').value = string;
+        }
+        else{
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
+    
+display.innerText = display.innerText.slice(0, -1)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
