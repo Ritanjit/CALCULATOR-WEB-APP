984a84af9c89d855d6627fd4c825e779ab04ec38
ritanjitdas@gmail.com <ritanjitdas@gmail.com>
2024-09-23 23:10:12 +0530

    all

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-let string ="";
+let tempp = ''
+let string = "";
 let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-    button.addEventListener('click',(e)=>{
-        if(e.target.innerHTML == '='){
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
             string = eval(string);
             document.querySelector('input').value = string;
         }
-        else if(e.target.innerHTML == 'AC'){
+        else if (e.target.innerHTML == 'AC') {
             string = "";
             document.querySelector('input').value = string;
         }
-        else if(e.target.innerHTML == '←'){
-            string = slice(0,-1);
+        else if (e.target.innerHTML == '←') {
+            string = slice(0, -1);
             document.querySelector('input').value = string;
         }
-        else{
+        else {
             console.log(e.target)
             string = string + e.target.innerHTML;
             document.querySelector('input').value = string;
         }
     })
 })
-    
+
 display.innerText = display.innerText.slice(0, -1)
 
-    
+
 
 
 
